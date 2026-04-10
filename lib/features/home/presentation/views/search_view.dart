610f4b0f4f855bf3a72a5f3319e0861091111d94
Karim Motaz <karemmotaz2929@gmail.com>
2026-04-10 23:14:45 +0200

    Finish Search View UI

--- lib/features/home/presentation/views/search_view.dart
+++ lib/features/home/presentation/views/search_view.dart
@@ -1,11 +1,95 @@
+import 'package:bazar_app/core/helpers/constants.dart';
+import 'package:bazar_app/core/theming/app_assets.dart';
+import 'package:bazar_app/core/theming/app_colors.dart';
+import 'package:bazar_app/core/theming/app_styles.dart';
 import 'package:bazar_app/features/home/presentation/widgets/custom_back_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-class SearchView extends StatelessWidget {
+class SearchView extends StatefulWidget {
   const SearchView({super.key});
 
   @override
+  State<SearchView> createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<SearchView> {
+  late TextEditingController searchController;
+
+  @override
+  void initState() {
+    searchController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: CustomBackAppbar(title: "Search"));
+    return Scaffold(
+      appBar: CustomBackAppbar(title: "Search"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kAppHorizontalPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 10),
+            TextField(
+              controller: searchController,
+              cursorColor: AppColors.mainColor,
+              decoration: InputDecoration(
+                hintText: "Search",
+                hintStyle: AppStyles.medium16.copyWith(color: Colors.grey),
+                prefixIcon: SvgPicture.asset(
+                  AppAssets.searchIcon,
+                  fit: BoxFit.scaleDown,
+                  height: 24,
+                  width: 24,
+                  colorFilter: const ColorFilter.mode(
+                    Colors.grey,
+                    BlendMode.srcIn,
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.grey.shade50,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(color: Colors.grey.shade300),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(color: Colors.grey.shade300),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(color: Colors.grey.shade300),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Text("Recent Searches", style: AppStyles.bold16),
+            const SizedBox(height: 24),
+            Column(
+              spacing: 32,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "The Good Sister",
+                  style: AppStyles.regular16.copyWith(color: Color(0xff8B8B97)),
+                ),
+                Text(
+                  "Carries Fisher",
+                  style: AppStyles.regular16.copyWith(color: Color(0xff8B8B97)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
